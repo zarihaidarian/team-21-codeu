79de80575104e76b90341ae0ca7e44464e2bb639
Zari Haidarian <zarihaidarian@gmail.com>
2019-06-14 14:34:03 -0700

    Added Styled Text to user page.

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -76,7 +76,7 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
-    String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String userText = Jsoup.clean(request.getParameter("text"), Whitelist.basicWithImages());
 
     String regex = "(https?://\\S+\\.(png|jpg))";
     String replacement = "<img src = \"$1\" />";
